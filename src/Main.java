3a44364546ad6200268c50ec3e2580c324d28b46
Paul <chupaul92@yahoo.com>
2020-04-19 12:03:54 +0200

    name and age

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
 public class Main {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String name;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    String age;
+
+
 
     public static void main(String[] args) {
-	// write your code here
+
+
         System.out.println("Hello World");
     }
+
+
 }
